70df904273c4e72d92e4c190dc2ebb2657605b80
Андрей Симоненко <simonenkoff.a@hotmail.com>
2019-06-19 16:19:27 +0300

    Start project

---
+++ ConvertorHEXtoDECIMAL/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ConvertorHEXtoDECIMAL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbHex = new System.Windows.Forms.TextBox();
+            this.tbDecimal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnOpenCom = new System.Windows.Forms.Button();
+            this.lbChekComPort = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbHex
+            // 
+            this.tbHex.Location = new System.Drawing.Point(26, 71);
+            this.tbHex.Name = "tbHex";
+            this.tbHex.Size = new System.Drawing.Size(233, 26);
+            this.tbHex.TabIndex = 0;
+            // 
+            // tbDecimal
+            // 
+            this.tbDecimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbDecimal.Location = new System.Drawing.Point(29, 151);
+            this.tbDecimal.Name = "tbDecimal";
+            this.tbDecimal.Size = new System.Drawing.Size(230, 44);
+            this.tbDecimal.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(24, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Дані з рідера(HEX)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(25, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(305, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Дані для SYSTEMA (DEC)";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConvert.Location = new System.Drawing.Point(265, 71);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(120, 38);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Конветувати";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(265, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 44);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Копіювати";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnExit.Location = new System.Drawing.Point(29, 204);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(190, 38);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Вихід";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnOpenCom
+            // 
+            this.btnOpenCom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnOpenCom.Location = new System.Drawing.Point(225, 204);
+            this.btnOpenCom.Name = "btnOpenCom";
+            this.btnOpenCom.Size = new System.Drawing.Size(160, 38);
+            this.btnOpenCom.TabIndex = 7;
+            this.btnOpenCom.Text = "Відкрити COM2";
+            this.btnOpenCom.UseVisualStyleBackColor = true;
+            this.btnOpenCom.Click += new System.EventHandler(this.BtnOpenCom_Click);
+            // 
+            // lbChekComPort
+            // 
+            this.lbChekComPort.AutoSize = true;
+            this.lbChekComPort.Location = new System.Drawing.Point(30, 10);
+            this.lbChekComPort.Name = "lbChekComPort";
+            this.lbChekComPort.Size = new System.Drawing.Size(164, 20);
+            this.lbChekComPort.TabIndex = 8;
+            this.lbChekComPort.Text = "Первірка COM порту";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(30, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "© 2019 Симоненко Андрій";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 264);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbChekComPort);
+            this.Controls.Add(this.btnOpenCom);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbDecimal);
+            this.Controls.Add(this.tbHex);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ConvertHEX2DEC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbHex;
+        private System.Windows.Forms.TextBox tbDecimal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnOpenCom;
+        private System.Windows.Forms.Label lbChekComPort;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ConvertorHEXtoDECIMAL/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ConvertorHEXtoDECIMAL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click_1(object sender, EventArgs e)
+        {
+            string hexValue = tbHex.Text;
+            long decValue = Int64.Parse(hexValue, System.Globalization.NumberStyles.HexNumber);
+            tbDecimal.Text = Convert.ToString(decValue);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            Clipboard.SetText(tbDecimal.Text);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)  
+        {
+            this.Close();
+        }
+
+        private void BtnOpenCom_Click(object sender, EventArgs e)
+        {
+            SerialPort serialPort = new SerialPort("COM2", 2400, Parity.None, 8, StopBits.One);
+            serialPort.Open();
+            bool _open = true;
+            
+            if(serialPort.IsOpen)
+            {
+                lbChekComPort.Text = "COM2 відкритий";
+                while (_open)
+                {
+                    try
+                    {
+                        string massege = serialPort.ReadLine();
+                        tbHex.Text = massege;
+                    }
+                    catch(TimeoutException)
+                    {
+
+                    }
+                    finally
+                    {
+                        _open = false;
+                    }   
+                }
+            }
+            else
+            {
+                lbChekComPort.Text = "COM2 закритий";
+            }
+        }
+
+
+        /*private void tbHex_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char l = e.KeyChar;
+            if (l < 'A' || l > 'Z' && l != ',' && l != ' ')
+            {
+                e.Handled = true;
+            }
+        }*/
+
+    }
+}
